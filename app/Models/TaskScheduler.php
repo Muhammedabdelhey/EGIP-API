451220d905ed1af58116b9f90be119e96237ca87
Mohamed Wazery <mohamedwazery03@gmail.com>
2023-03-28 23:15:50 +0200

    add fillable to tasks model

--- app/Models/TaskScheduler.php
+++ app/Models/TaskScheduler.php
@@ -9,5 +9,6 @@ class TaskScheduler extends Model
 {
     use HasFactory;
     protected $table="task_schedulers";
+    protected $fillable=['name','details','time','repeat','repeats_per_day','times_of_repeat','patient_id'];
     public $timestamps=false;
 }
